7b2c8763757964cecd0b09b191ee35cf8bb3cc83
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-23 20:51:13 +0200

    Added the db context file and configured the appsettings.json and edited the Startup.cs.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,5 +9,18 @@ namespace Etaa.Data
             : base(options)
         {
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
     }
 }
