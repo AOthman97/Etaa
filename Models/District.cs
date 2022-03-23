78553d1c53ff6177ffbc0ffba1ccb4d565b766ca
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-23 21:10:33 +0200

    Added district model

---
+++ Models/District.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Etaa.Models
+{
+    public class District
+    {
+        [Key]
+        public int DistrictId { get; set; }
+        [Required(ErrorMessage = "Name(Ar) is Required!")]
+        [Display(Name = "Name(Ar)")]
+        public string NameAr { get; set; }
+        public string? NameEn { get; set; }
+
+        // Relationship between the movie and the cinema
+        public int CityNo { get; set; }
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+    }
+}
