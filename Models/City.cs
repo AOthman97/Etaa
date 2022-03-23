8415033e8ca39fc2570811964bb5debe1e077c06
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-23 21:08:56 +0200

    Added city model

---
+++ Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Etaa.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+        [Required(ErrorMessage = "Name(Ar) is Required!")]
+        [Display(Name = "Name(Ar)")]
+        public string NameAr { get; set; }
+        public string? NameEn { get; set; }
+
+        // Relationship between the movie and the cinema
+        public int StateNo { get; set; }
+        [ForeignKey("StateId")]
+        public State State { get; set; }
+    }
+}
