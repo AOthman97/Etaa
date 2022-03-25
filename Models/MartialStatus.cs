b0f02d6cad565e9c1d79b65383bec1ed4c82502f
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-25 11:04:19 +0200

    Added the martial status model.

---
+++ Models/MartialStatus.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Etaa.Models
+{
+    public class MartialStatus
+    {
+        [Key]
+        public int MartialStatusId { get; set; }
+        [Required(ErrorMessage = "Name(Ar) is Required!")]
+        [Display(Name = "Name(Ar)")]
+        public string NameAr { get; set; }
+        public string? NameEn { get; set; }
+
+        // Each state contains a number of cities, This property defines the relationship between them
+        public ICollection<FamilyMember>? FamilyMembers;
+    }
+}
