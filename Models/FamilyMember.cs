051fd0f10d5fb24c7b0845b4c0ed4b739a122f57
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-25 10:46:39 +0200

    Added the kinship and family member models and edited them to handle the relationship between them.

---
+++ Models/FamilyMember.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Etaa.Models
+{
+    public class FamilyMember
+    {
+        [Key]
+        public int FamilyMemberId { get; set; }
+        [Required(ErrorMessage = "Name(Ar) is Required!")]
+        [Display(Name = "Name(Ar)")]
+        public string NameAr { get; set; }
+        public string? NameEn { get; set; }
+        public int Age { get; set; }
+        public string Note { get; set; }
+
+        // Relationship between the family member and kinship
+        public int KinshipId { get; set; }
+        [ForeignKey("KinshipId")]
+        public Kinship Kinship { get; set; }
+    }
+}
--- Models/Kinship.cs
+++ Models/Kinship.cs
@@ -1,7 +1,18 @@
-﻿namespace Etaa.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Etaa.Models
 {
     public class Kinship
     {
+        [Key]
+        public int KinshipId { get; set; }
+        [Required(ErrorMessage = "Name(Ar) is Required!")]
+        [Display(Name = "Name(Ar)")]
+        public string NameAr { get; set; }
+        public string? NameEn { get; set; }
 
+        // Each kinship contains a number of family members, This property defines the relationship
+        // between them
+        public ICollection<FamilyMember> FamilyMembers;
     }
 }
