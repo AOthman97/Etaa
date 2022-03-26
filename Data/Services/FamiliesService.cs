2a6f155680b614b43fcd6406533bc53691fb40c2
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-26 09:01:39 +0200

    Added the Interface and the service for the family model.

---
+++ Data/Services/FamiliesService.cs
@@ -0,0 +1,39 @@
+﻿using Etaa.Models;
+
+namespace Etaa.Data.Services
+{
+    public class FamiliesService : IFamiliesService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public FamiliesService(ApplicationDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+
+        public Task AddFamilyAsync(Family family)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteFamily(int FamilyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Family>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Family> GetFamilyAsync(int FamilyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Family> UpdateFamilyAsync(int FamilyId, Family family)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Services/IFamiliesService.cs
@@ -0,0 +1,13 @@
+﻿using Etaa.Models;
+
+namespace Etaa.Data.Services
+{
+    public interface IFamiliesService
+    {
+        public Task<IEnumerable<Family>> GetAllAsync();
+        Task<Family> GetFamilyAsync(int FamilyId);
+        Task AddFamilyAsync(Family family);
+        Task<Family> UpdateFamilyAsync(int FamilyId, Family family);
+        bool DeleteFamily(int FamilyId);
+    }
+}
