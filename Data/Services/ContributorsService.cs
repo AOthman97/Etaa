730ff1082cc5e31111b829b5def382223c477da0
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-04-05 06:53:05 +0200

    Added the methods for the contributor service

--- Data/Services/ContributorsService.cs
+++ Data/Services/ContributorsService.cs
@@ -1,30 +1,43 @@
 ﻿using Etaa.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Etaa.Data.Services
 {
     public class ContributorsService : IContributorsService
     {
-        public Task AddContributorAsync(Contributor contributor)
+        private readonly ApplicationDbContext _dbContext;
+
+        public ContributorsService(ApplicationDbContext applicationDbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = applicationDbContext;
+        }
+
+        public async Task AddContributorAsync(Contributor contributor)
+        {
+            var Result = await _dbContext.Contributors.AddAsync(contributor);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Contributor>> GetAllAsync()
+        {
+            var Result = await _dbContext.Contributors.OrderBy(Contributor => Contributor.NameAr).ToListAsync();
+            return Result;
+        }
+
+        public async Task<Contributor> GetContributorAsync(int ContributorId)
+        {
+            var Result = await _dbContext.Contributors.FirstOrDefaultAsync(Contributor => Contributor.ContributorId == ContributorId);
+            return Result;
+        }
+
+        public async Task<Contributor> UpdateContributorAsync(int ContributorId, Contributor contributor)
+        {
+            _dbContext.Update(contributor);
+            await _dbContext.SaveChangesAsync();
+            return contributor;
         }
 
         public bool DeleteContributor(int ContributorId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Contributor>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Contributor> GetContributorAsync(int ContributorId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Contributor> UpdateContributorAsync(int ContributorId, Contributor contributor)
         {
             throw new NotImplementedException();
         }
