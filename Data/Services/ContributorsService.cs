d86676cb1393c342a951be9a16dcd5532910604c
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-04-05 06:47:38 +0200

    Added the Contributors service.

---
+++ Data/Services/ContributorsService.cs
@@ -0,0 +1,32 @@
+﻿using Etaa.Models;
+
+namespace Etaa.Data.Services
+{
+    public class ContributorsService : IContributorsService
+    {
+        public Task AddContributorAsync(Contributor contributor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteContributor(int ContributorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contributor>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contributor> GetContributorAsync(int ContributorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contributor> UpdateContributorAsync(int ContributorId, Contributor contributor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
