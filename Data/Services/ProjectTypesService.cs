9b69b5865a67e60717bbb2d24614f14f32d199fa
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-04-22 22:17:09 +0200

    Added the ProjectTypesService.

---
+++ Data/Services/ProjectTypesService.cs
@@ -0,0 +1,27 @@
+﻿using Etaa.Models;
+
+namespace Etaa.Data.Services
+{
+    public class ProjectTypesService : IProjectTypesService
+    {
+        public Task<int> AddProjectTypeAsync(IEnumerable<ProjectTypes> ProjectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProjectType(int ProjectTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProjectTypes>> GetProjectTypeAsync(int ProjectTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectTypes> UpdateProjectTypeAsync(int ProjectTypeId, ProjectTypes ProjectTypes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
