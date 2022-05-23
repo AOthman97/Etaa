99505d2051953e32b04aa7149a7c39196abd8b64
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-05-23 21:56:42 +0200

    Added try_catch to ProjectTypesAssetController.

--- Controllers/ProjectTypesAssetController.cs
+++ Controllers/ProjectTypesAssetController.cs
@@ -23,48 +23,69 @@ namespace Etaa.Controllers
         // GET: ProjectTypesAsset
         public async Task<IActionResult> Index(int ProjectTypeId)
         {
-            List<ProjectTypesAssets> ProjectTypesAssets = await (from ProjectTypesAsset in _context.ProjectTypesAssets
-                                                                 where ProjectTypesAsset.ProjectTypeId == ProjectTypeId
-                                                                 select new ProjectTypesAssets
-                                                                 {
-                                                                     ProjectTypesAssetsId = ProjectTypesAsset.ProjectTypesAssetsId,
-                                                                     NameAr = ProjectTypesAsset.NameAr,
-                                                                     NameEn = ProjectTypesAsset.NameEn,
-                                                                     ProjectTypeId = ProjectTypeId
-                                                                 }).ToListAsync();
+            try
+            {
+                List<ProjectTypesAssets> ProjectTypesAssets = await (from ProjectTypesAsset in _context.ProjectTypesAssets
+                                                                     where ProjectTypesAsset.ProjectTypeId == ProjectTypeId
+                                                                     select new ProjectTypesAssets
+                                                                     {
+                                                                         ProjectTypesAssetsId = ProjectTypesAsset.ProjectTypesAssetsId,
+                                                                         NameAr = ProjectTypesAsset.NameAr,
+                                                                         NameEn = ProjectTypesAsset.NameEn,
+                                                                         ProjectTypeId = ProjectTypeId
+                                                                     }).ToListAsync();
 
-            ViewBag.ProjectTypeId = ProjectTypeId;
-            // await _context.ProjectTypesAssets.ToListAsync()
-            return View(ProjectTypesAssets);
+                ViewBag.ProjectTypeId = ProjectTypeId;
+                // await _context.ProjectTypesAssets.ToListAsync()
+                return View(ProjectTypesAssets);
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: ProjectTypesAsset/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var projectTypesAssets = await _context.ProjectTypesAssets
-                .FirstOrDefaultAsync(m => m.ProjectTypesAssetsId == id);
-            if (projectTypesAssets == null)
+                var projectTypesAssets = await _context.ProjectTypesAssets
+                    .FirstOrDefaultAsync(m => m.ProjectTypesAssetsId == id);
+                if (projectTypesAssets == null)
+                {
+                    return NotFound();
+                }
+
+                return View(projectTypesAssets);
+            }
+            catch (Exception ex)
             {
-                return NotFound();
+                return View("Error");
             }
-
-            return View(projectTypesAssets);
         }
 
         // GET: ProjectTypesAsset/Create
         public IActionResult Create(int ProjectTypeId)
         {
-            ProjectTypesAssets ProjectTypesAssets = new ProjectTypesAssets
+            try
             {
-                ProjectTypeId = ProjectTypeId
-            };
+                ProjectTypesAssets ProjectTypesAssets = new ProjectTypesAssets
+                {
+                    ProjectTypeId = ProjectTypeId
+                };
 
-            return View(ProjectTypesAssets);
+                return View(ProjectTypesAssets);
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // POST: ProjectTypesAsset/Create
@@ -74,31 +95,45 @@ namespace Etaa.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ProjectTypesAssetsId,NameAr,NameEn,IsCanceled,ProjectTypeId")] ProjectTypesAssets projectTypesAssets)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(projectTypesAssets);
-                await _context.SaveChangesAsync();
-                // When the view just returned the Index the category items weren't shown, That's because we didn't pass-in
-                // the ProjectTypeId to select from that's used in the Index action method
-                return RedirectToAction(nameof(Index), new { ProjectTypeId = projectTypesAssets.ProjectTypeId });
+                if (ModelState.IsValid)
+                {
+                    _context.Add(projectTypesAssets);
+                    await _context.SaveChangesAsync();
+                    // When the view just returned the Index the category items weren't shown, That's because we didn't pass-in
+                    // the ProjectTypeId to select from that's used in the Index action method
+                    return RedirectToAction(nameof(Index), new { ProjectTypeId = projectTypesAssets.ProjectTypeId });
+                }
+                return View(projectTypesAssets);
             }
-            return View(projectTypesAssets);
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: ProjectTypesAsset/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var projectTypesAssets = await _context.ProjectTypesAssets.FindAsync(id);
-            if (projectTypesAssets == null)
-            {
-                return NotFound();
+                var projectTypesAssets = await _context.ProjectTypesAssets.FindAsync(id);
+                if (projectTypesAssets == null)
+                {
+                    return NotFound();
+                }
+                return View(projectTypesAssets);
             }
-            return View(projectTypesAssets);
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // POST: ProjectTypesAsset/Edit/5
@@ -108,50 +143,64 @@ namespace Etaa.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ProjectTypesAssetsId,NameAr,NameEn,IsCanceled,ProjectTypeId")] ProjectTypesAssets projectTypesAssets)
         {
-            if (id != projectTypesAssets.ProjectTypesAssetsId)
+            try
             {
-                return NotFound();
-            }
+                if (id != projectTypesAssets.ProjectTypesAssetsId)
+                {
+                    return NotFound();
+                }
 
-            if (ModelState.IsValid)
-            {
-                try
+                if (ModelState.IsValid)
                 {
-                    _context.Update(projectTypesAssets);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProjectTypesAssetsExists(projectTypesAssets.ProjectTypesAssetsId))
+                    try
                     {
-                        return NotFound();
+                        _context.Update(projectTypesAssets);
+                        await _context.SaveChangesAsync();
                     }
-                    else
+                    catch (DbUpdateConcurrencyException)
                     {
-                        throw;
+                        if (!ProjectTypesAssetsExists(projectTypesAssets.ProjectTypesAssetsId))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
+                    return RedirectToAction(nameof(Index));
                 }
-                return RedirectToAction(nameof(Index));
+                return View(projectTypesAssets);
             }
-            return View(projectTypesAssets);
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: ProjectTypesAsset/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var projectTypesAssets = await _context.ProjectTypesAssets
-                .FirstOrDefaultAsync(m => m.ProjectTypesAssetsId == id);
-            if (projectTypesAssets == null)
+                var projectTypesAssets = await _context.ProjectTypesAssets
+                    .FirstOrDefaultAsync(m => m.ProjectTypesAssetsId == id);
+                if (projectTypesAssets == null)
+                {
+                    return NotFound();
+                }
+
+                return View(projectTypesAssets);
+            }
+            catch (Exception ex)
             {
-                return NotFound();
+                return View("Error");
             }
-
-            return View(projectTypesAssets);
         }
 
         // POST: ProjectTypesAsset/Delete/5
@@ -159,15 +208,29 @@ namespace Etaa.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var projectTypesAssets = await _context.ProjectTypesAssets.FindAsync(id);
-            _context.ProjectTypesAssets.Remove(projectTypesAssets);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                var projectTypesAssets = await _context.ProjectTypesAssets.FindAsync(id);
+                _context.ProjectTypesAssets.Remove(projectTypesAssets);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         private bool ProjectTypesAssetsExists(int id)
         {
-            return _context.ProjectTypesAssets.Any(e => e.ProjectTypesAssetsId == id);
+            try
+            {
+                return _context.ProjectTypesAssets.Any(e => e.ProjectTypesAssetsId == id);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
