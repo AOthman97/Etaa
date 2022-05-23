213818a2b989f31bd091d09ad6290ec95dafedc2
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-05-23 21:53:34 +0200

    Added try_catch to ProjectTypeController.

--- Controllers/ProjectTypeController.cs
+++ Controllers/ProjectTypeController.cs
@@ -23,36 +23,57 @@ namespace Etaa.Controllers
         // GET: ProjectType
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.ProjectTypes.Include(p => p.ProjectDomainTypes).Include(p => p.ProjectGroup);
-            return View(await applicationDbContext.ToListAsync());
+            try
+            {
+                var applicationDbContext = _context.ProjectTypes.Include(p => p.ProjectDomainTypes).Include(p => p.ProjectGroup);
+                return View(await applicationDbContext.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: ProjectType/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var projectTypes = await _context.ProjectTypes
-                .Include(p => p.ProjectDomainTypes)
-                .Include(p => p.ProjectGroup)
-                .FirstOrDefaultAsync(m => m.ProjectTypeId == id);
-            if (projectTypes == null)
+                var projectTypes = await _context.ProjectTypes
+                    .Include(p => p.ProjectDomainTypes)
+                    .Include(p => p.ProjectGroup)
+                    .FirstOrDefaultAsync(m => m.ProjectTypeId == id);
+                if (projectTypes == null)
+                {
+                    return NotFound();
+                }
+
+                return View(projectTypes);
+            }
+            catch (Exception ex)
             {
-                return NotFound();
+                return View("Error");
             }
-
-            return View(projectTypes);
         }
 
         // GET: ProjectType/Create
         public IActionResult Create()
         {
-            ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr");
-            ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr");
-            return View();
+            try
+            {
+                ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr");
+                ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr");
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // POST: ProjectType/Create
@@ -62,33 +83,47 @@ namespace Etaa.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ProjectTypeId,NameAr,NameEn,IsCanceled,ProjectDomainTypeId,ProjectGroupId")] ProjectTypes projectTypes)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(projectTypes);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(projectTypes);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr", projectTypes.ProjectDomainTypeId);
+                ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr", projectTypes.ProjectGroupId);
+                return View(projectTypes);
             }
-            ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr", projectTypes.ProjectDomainTypeId);
-            ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr", projectTypes.ProjectGroupId);
-            return View(projectTypes);
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: ProjectType/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var projectTypes = await _context.ProjectTypes.FindAsync(id);
-            if (projectTypes == null)
-            {
-                return NotFound();
+                var projectTypes = await _context.ProjectTypes.FindAsync(id);
+                if (projectTypes == null)
+                {
+                    return NotFound();
+                }
+                ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr", projectTypes.ProjectDomainTypeId);
+                ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr", projectTypes.ProjectGroupId);
+                return View(projectTypes);
             }
-            ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr", projectTypes.ProjectDomainTypeId);
-            ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr", projectTypes.ProjectGroupId);
-            return View(projectTypes);
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // POST: ProjectType/Edit/5
@@ -98,54 +133,68 @@ namespace Etaa.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ProjectTypeId,NameAr,NameEn,IsCanceled,ProjectDomainTypeId,ProjectGroupId")] ProjectTypes projectTypes)
         {
-            if (id != projectTypes.ProjectTypeId)
+            try
             {
-                return NotFound();
-            }
+                if (id != projectTypes.ProjectTypeId)
+                {
+                    return NotFound();
+                }
 
-            if (ModelState.IsValid)
-            {
-                try
+                if (ModelState.IsValid)
                 {
-                    _context.Update(projectTypes);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProjectTypesExists(projectTypes.ProjectTypeId))
+                    try
                     {
-                        return NotFound();
+                        _context.Update(projectTypes);
+                        await _context.SaveChangesAsync();
                     }
-                    else
+                    catch (DbUpdateConcurrencyException)
                     {
-                        throw;
+                        if (!ProjectTypesExists(projectTypes.ProjectTypeId))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
+                    return RedirectToAction(nameof(Index));
                 }
-                return RedirectToAction(nameof(Index));
+                ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr", projectTypes.ProjectDomainTypeId);
+                ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr", projectTypes.ProjectGroupId);
+                return View(projectTypes);
             }
-            ViewData["ProjectDomainTypeId"] = new SelectList(_context.ProjectDomainTypes, "ProjectDomainTypeId", "NameAr", projectTypes.ProjectDomainTypeId);
-            ViewData["ProjectGroupId"] = new SelectList(_context.ProjectGroups, "ProjectGroupId", "NameAr", projectTypes.ProjectGroupId);
-            return View(projectTypes);
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: ProjectType/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
+                if (id == null)
+                {
+                    return NotFound();
+                }
 
-            var projectTypes = await _context.ProjectTypes
-                .Include(p => p.ProjectDomainTypes)
-                .Include(p => p.ProjectGroup)
-                .FirstOrDefaultAsync(m => m.ProjectTypeId == id);
-            if (projectTypes == null)
+                var projectTypes = await _context.ProjectTypes
+                    .Include(p => p.ProjectDomainTypes)
+                    .Include(p => p.ProjectGroup)
+                    .FirstOrDefaultAsync(m => m.ProjectTypeId == id);
+                if (projectTypes == null)
+                {
+                    return NotFound();
+                }
+
+                return View(projectTypes);
+            }
+            catch (Exception ex)
             {
-                return NotFound();
+                return View("Error");
             }
-
-            return View(projectTypes);
         }
 
         // POST: ProjectType/Delete/5
@@ -153,15 +202,29 @@ namespace Etaa.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var projectTypes = await _context.ProjectTypes.FindAsync(id);
-            _context.ProjectTypes.Remove(projectTypes);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                var projectTypes = await _context.ProjectTypes.FindAsync(id);
+                _context.ProjectTypes.Remove(projectTypes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         private bool ProjectTypesExists(int id)
         {
-            return _context.ProjectTypes.Any(e => e.ProjectTypeId == id);
+            try
+            {
+                return _context.ProjectTypes.Any(e => e.ProjectTypeId == id);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
