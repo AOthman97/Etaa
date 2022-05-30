bd5a1efc57be464b2ddcd39c078ff7549ba30d2a
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-05-30 17:00:51 +0200

    For the Family make the DistrictId nullable, also add a required attribute to both name attributes.

---
+++ Data/Migrations/20220530145927_EditNullableOfPropertiesInFamilyModel.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Etaa.Data.Migrations
+{
+    public partial class EditNullableOfPropertiesInFamilyModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Families_Districts_DistrictId",
+                table: "Families");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameEn",
+                table: "Families",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCurrentInvestmentProject",
+                table: "Families",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DistrictId",
+                table: "Families",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Families_Districts_DistrictId",
+                table: "Families",
+                column: "DistrictId",
+                principalTable: "Districts",
+                principalColumn: "DistrictId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Families_Districts_DistrictId",
+                table: "Families");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameEn",
+                table: "Families",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCurrentInvestmentProject",
+                table: "Families",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DistrictId",
+                table: "Families",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Families_Districts_DistrictId",
+                table: "Families",
+                column: "DistrictId",
+                principalTable: "Districts",
+                principalColumn: "DistrictId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
