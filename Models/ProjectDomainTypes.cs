823e5e9966d0a0de8793d577c5961b95d8a91c63
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-06-04 21:27:05 +0200

    Added the ar lang to PaymentVoucher.

--- Models/ProjectDomainTypes.cs
+++ Models/ProjectDomainTypes.cs
@@ -6,8 +6,8 @@ namespace Etaa.Models
     {
         [Key]
         public int ProjectDomainTypeId { get; set; }
-        [Required(ErrorMessage = "Name(Ar) is Required!")]
-        [Display(Name = "Name(Ar)")]
+        [Required(ErrorMessage = "الإسم عربي مطلوب !")]
+        [Display(Name = "الإسم عربي")]
         public string NameAr { get; set; }
         public string? NameEn { get; set; }
         public bool? IsCanceled { get; set; }
