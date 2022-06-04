99ccec07333fc096ec37c3ce9cb75a6d14a0cb8d
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-06-04 19:45:14 +0200

    Added ar lang to the Clearance model all views, Also edited the nullable of the IsCanceled and IsApprovedByManagement properties.

--- Models/Clearance.cs
+++ Models/Clearance.cs
@@ -7,19 +7,26 @@ namespace Etaa.Models
     {
         [Key]
         public int ClearanceId { get; set; }
+        [Display(Name = "مستند خلو الطرف")]
         public string? ClearanceDocumentPath { get; set; }
+        [Display(Name = "التعليقات")]
         public string? Comments { get; set; }
         [Display(Name = "تاريخ خلو الطرف")]
         [DataType(DataType.Date)]
         public DateTime ClearanceDate { get; set; }
-        public bool? IsApprovedByManagement { get; set; }
-        public bool? IsCanceled { get; set; }
+        [Display(Name = "هل تمت الموافقة من قبل الإدارة")]
+        public bool IsApprovedByManagement { get; set; }
+        [Display(Name = "هل تم الحذف")]
+        public bool IsCanceled { get; set; }
 
         // Relationship between the projects and families
+        [Display(Name = "المشروع")]
         public int ProjectId { get; set; }
         [ForeignKey("ProjectId")]
         public Projects? Projects { get; set; }
+        [Display(Name = "إسم المستخدم")]
         public string? UserId { get; set; }
+        [Display(Name = "إسم مستخدم الإدارة")]
         public string? ManagementUserId { get; set; }
     }
 }
